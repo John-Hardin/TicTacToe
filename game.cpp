bfbd796be544e521204cf1a0be0df450d36fc74f
Swan <hardinjohn166@gmail.com>
2021-03-22 08:44:20 -0400

    --no-edit

--- game.cpp
+++ game.cpp
@@ -35,27 +35,27 @@ void Game::updateGame(bool& gO){
     exit(1);
 }
 
-int Game::setPlayerAmount(std::string playerAmountString){ 
-const unsigned int max_players = 4;
-std::string numbers_text[] = {"one", "two", "three", "four"};
-std::map<int, std::regex> conversion_table = get_regex_map(max_players, numbers_text); 
-std::cout << "Enter (1) Player or (2) Players?" << std::endl;
-std::cin >> playerAmountString;
+void Game::setPlayerAmount(std::string playerAmountString){ 
+    const unsigned int max_players = 4;
+    std::string numbers_text[] = {"one", "two", "three", "four"};
+    std::map<int, std::regex> conversion_table = get_regex_map(max_players, numbers_text); 
+    std::cout << "Enter (1) Player or (2) Players?" << std::endl;
+    std::cin >> playerAmountString;
 
-while(!std::cin.fail() && std::regex_match(playerAmountString, getRegex(max_players, *numbers_text))){ 
+        while(!std::cin.fail() && std::regex_match(playerAmountString, getRegex(max_players, *numbers_text))){ 
 
-        for(auto &el: conversion_table){  
-            if (std::regex_match(playerAmountString, el.second)){
-                std::cout << "Number of players selected is : " << el.first << std::endl;
-                playerAmount_ = el.first;
-                std::cout << "TEST 1 : playerAmount_ is : " << playerAmount_ << std::endl;
+                for(auto &el: conversion_table){  
+                    if (std::regex_match(playerAmountString, el.second)){
+                        std::cout << "Number of players selected is : " << el.first << std::endl;
+                        playerAmount_ = el.first;
+                        std::cout << "TEST 1 : playerAmount_ is : " << playerAmount_ << std::endl;
 
-            } 
+                    } 
+                }
+                std::cout << "TEST 2 : playerAmount_ is : " << playerAmount_ << std::endl;
+                return;
         }
-        std::cout << "TEST 2 : playerAmount_ is : " << playerAmount_ << std::endl;
-        return playerAmount_;
-}
-return 1;
+
 }
 
 void Game::setPlayerNames(std::vector<std::string> &playerNames){ //todo -- Look into "std::unique_ptr<T[]>" instead of vector.
@@ -64,7 +64,7 @@ void Game::setPlayerNames(std::vector<std::string> &playerNames){ //todo -- Look
     std::cout << "TEST 1 : getPlayerAmount() is : " << getPlayerAmount() << std::endl;
     std::cout << "TEST 2 : l_playerAmount = " << l_playerAmount << std::endl;
     std::string pName;
-    
+
     for (int i = 1; i <= l_playerAmount; i++){  //TODO -- Look into "std::unique_ptr<T[]>" instead of vector.
         std::cout << "Enter player " << i << "'s name : " << std::endl;
         std::cin >> pName;
@@ -77,20 +77,22 @@ void Game::setPlayerNames(std::vector<std::string> &playerNames){ //todo -- Look
 }
 
 std::regex Game::getRegex(unsigned int max_players, std::string numbers_text){
-std::stringstream ss;
-ss << "[1-";
-ss << max_players;
-std::cout << "TEST : max_players is " << max_players << std::endl;
-ss << "]";
-for (unsigned int i = 0; i < max_players; i++){
-    ss << "|";
-    ss << numbers_text[i];
-}
-std::string s = "";
-ss >> s;
-std::cout << "TEST 2 : s is : " << s << std::endl;
-std::regex regex(s, std::regex::icase);
-return regex;
+    std::stringstream ss;
+    ss << "[1-";
+    ss << max_players;
+    std::cout << "TEST : max_players is " << max_players << std::endl;
+    ss << "]";
+
+    for (unsigned int i = 0; i < max_players; i++){
+        ss << "|";
+        ss << numbers_text[i];
+    }
+
+    std::string s = "";
+    ss >> s;
+    std::cout << "TEST 2 : s is : " << s << std::endl;
+    std::regex regex(s, std::regex::icase);
+    return regex;
 }
 
 std::map<int, std::regex> Game::get_regex_map(unsigned int max_players, std::string* numbers_text){
