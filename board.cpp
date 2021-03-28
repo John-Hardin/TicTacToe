d9951662ebbe3756bdf09eb4dde85c3641e0bdd7
Swan <hardinjohn166@gmail.com>
2021-03-28 11:35:54 -0400

    probably don't need these

--- board.cpp
+++ board.cpp
@@ -1,18 +1,1 @@
 #include "board.hpp"
-
-
-#include <iostream>
-#include <array>
-
-    Board::Board() {  //todo, not using player from board.hpp this way, change this.
-        // todo Call initBoard when function is completely writtened'd.
-
-
-    }
-    Board::~Board(){
-
-    }
-    void Board::initBoard(){ //todo create a player array
-        //check if 3x3 or 5x5 game
-
-    }
