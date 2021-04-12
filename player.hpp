f7fbe94069a98ca7cdb222606f8bee22dc5fd21d
Swan <hardinjohn166@gmail.com>
2021-04-12 09:08:41 -0400

    Whatever.

--- player.hpp
+++ player.hpp
@@ -7,7 +7,7 @@
 struct Player
 {   
 //constructors, copys, moves, destructors
-    Player();
+    Player(){};
     Player( std::string _N, char _S){ std::cout << "Player is created : " << _N << " with symbol " << _S << std::endl; name_ = _N; symbol_ = _S; }
     Player(Player&&) = default;
     ~Player(){ std::cout << "Player " << name_ << " is destroyed." << std::endl; }
